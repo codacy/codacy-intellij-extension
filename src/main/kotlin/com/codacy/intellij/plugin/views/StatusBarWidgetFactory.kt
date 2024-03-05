6700f337c57d1d569a61d3cc3e7d636646dbe2c3
Ariel Kosacoff <ariel@shapert.com>
2024-03-04 22:20:34 -0300

    Remove comments

--- src/main/kotlin/com/codacy/intellij/plugin/views/StatusBarWidgetFactory.kt
+++ src/main/kotlin/com/codacy/intellij/plugin/views/StatusBarWidgetFactory.kt
@@ -15,22 +15,18 @@ class CodacyStatusBarWidgetFactory : StatusBarWidgetFactory {
     }
 
     override fun isAvailable(project: Project): Boolean {
-        // Implement logic to determine if the widget should be available for the given project
         return true
     }
 
     override fun createWidget(project: Project): StatusBarWidget {
-        // Create and return an instance of your CodacyStatusBarWidget
         return CodacyStatusBarWidget(project)
     }
 
     override fun disposeWidget(widget: StatusBarWidget) {
-        // Dispose of your widget if needed
         widget.dispose()
     }
 
     override fun canBeEnabledOn(statusBar: StatusBar): Boolean {
-        // Implement any logic if your widget can be enabled on the given status bar
         return true
     }
 }
